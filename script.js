03c01b367449b3935207e5c7893103277c86625a
RajulaLincy <rajulalincy@gmail.com>
2024-07-23 00:17:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 let yesButton = document.getElementById('yesButton');
 let noButton = document.getElementById('noButton');
-let noTexts = ["Are you sure?", "Really?", "Think again!", "Still No?", "Come on!", "Last chance!"];
+let noTexts = [
+    "Oh, come on!",
+    "Really? Try again!",
+    "You're not serious, right?",
+    "This isn't rocket science!",
+    "Who hurt you?",
+    "You can't be serious!",
+    "You must be joking!",
+    "Are you for real?",
+    "Don't be that guy!",
+    "Give it a shot!",
+    "Live a little!",
+    "Make up your mind!",
+    "Just say yes!",
+    "What are you afraid of?",
+    "You're missing out!",
+    "Seriously, just do it!",
+    "Be a hero!",
+    "Take the plunge!",
+    "Do it for the memes!",
+    "Don't overthink it!"
+];
 let noClickCount = 0;
 
 function handleYes() {
@@ -12,7 +33,7 @@ function handleNo() {
     if (noClickCount < noTexts.length) {
         noButton.textContent = noTexts[noClickCount];
     } else {
-        noButton.textContent = "Seriously?";
+        noButton.textContent = "Come on, just say yes!";
     }
     yesButton.style.fontSize = `${16 + noClickCount * 2}px`;
 }
